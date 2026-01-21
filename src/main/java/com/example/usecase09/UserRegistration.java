7b13de46064a1cf5d6a79ed9dc461bcf0524b830
Tomi Virtanen <tltv@vaadin.com>
2026-01-21 17:56:58 +0200

    chore: update use case 9: Binder
    Updated Binder use case to actually use Binder features. Updated to use current signals API.
    
    Fixes #9

--- src/main/java/com/example/usecase09/UserRegistration.java
+++ src/main/java/com/example/usecase09/UserRegistration.java
@@ -1,5 +1,72 @@
 package com.example.usecase09;
 
-public record UserRegistration(String username, String email, String password,
-        String confirmPassword, AccountType accountType, Integer age) {
+public class UserRegistration {
+
+    private String username;
+    private String email;
+    private String password;
+    private String confirmPassword;
+    private AccountType accountType;
+    private Integer age;
+
+    public UserRegistration() {
+    }
+
+    public UserRegistration(String username, String email, String password,
+            String confirmPassword, AccountType accountType, Integer age) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+        this.accountType = accountType;
+        this.age = age;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public AccountType getAccountType() {
+        return accountType;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    public void setAccountType(AccountType accountType) {
+        this.accountType = accountType;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
